f2008c4250a5b0255c318c6e761ba90de271e45d
Your name herecod3rcarl <your_email@example.comcod3rcarl@gmail.com>
2021-01-07 14:31:03 +0000

    backend changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
@@ -9,25 +8,20 @@ var usersRouter = require("./routes/users");
 var sessionRouter = require("./routes/session");
 
 var cors = require("cors");
-
 var app = express();
 
 // Handle cors
 app.use(cors());
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
 app.use("/session", sessionRouter);
 
 // catch 404 and forward to error handler
@@ -35,16 +29,16 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 app.listen(3001, () => {
   console.log("listening on port 3001");
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Callback Cats" });
+  res.json({ success: "Welcome to our App" });
 });
 
 module.exports = router;
