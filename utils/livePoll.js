be6fbf81d52d6aaec448293e2b8c94f14cd1fd4c
jimbowler82 <j.bowler2017@gmail.com>
2021-01-13 13:10:37 +0000

    index value corrections

--- utils/livePoll.js
+++ utils/livePoll.js
@@ -15,7 +15,7 @@ function getResultsData() {
 
 function updateResultsData(data) {
   const obj = resultsData.options.find((option) => option[0] === data);
-  if (obj) obj[3] = obj[3] += 1;
+  if (obj) obj[2] = obj[2] += 1;
 
   console.log("Updated results object: ", getResultsData());
 }
