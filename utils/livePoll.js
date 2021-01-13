123c941088943aa499aac90fa63f7b3a933fc05e
jimbowler82 <j.bowler2017@gmail.com>
2021-01-13 13:06:41 +0000

    adjust result update

--- utils/livePoll.js
+++ utils/livePoll.js
@@ -15,11 +15,7 @@ function getResultsData() {
 
 function updateResultsData(data) {
   const obj = resultsData.options.find((option) => option[0] === data);
-  if (obj[3] === undefined) {
-    obj[3] = 1;
-  } else {
-    obj[3] += 1;
-  }
+  if (obj) obj[3] = obj[3] += 1;
 
   console.log("Updated results object: ", getResultsData());
 }
