01b92225662ba8ec81edc0304805c98d9b303b81
Kunmi Williams <kumswilliams@gmail.com>
2021-01-14 17:19:51 +0000

    post request

--- routes/users.test.js
+++ routes/users.test.js
@@ -26,10 +26,27 @@ describe("GET /users/:id", () => {
       role: "bootcamper",
       cohortno: 4,
       email: "participantview@gmail.com",
+      message: `You have added a new user`,
     });
-    afterAll(async () => {
-      await db.close();
-      await app.close();
+  });
+
+  describe("POST /users", () => {
+    it("should post an object to the db", async () => {
+      const response = await request.post("/users");
+      expect(response.body).toMatchObject({
+        id: 56,
+        uuid: "8a6a31b4-1a6c-482a-bcfc-51cdba6bad10",
+        bootcamperid: "0000",
+        firstname: "Chris",
+        surname: "Meah",
+        role: "coach",
+        cohortno: 0,
+        email: "chris@chris.com",
+      });
     });
+  });
+  afterAll(async () => {
+    await db.close();
+    await app.close();
   });
 });
