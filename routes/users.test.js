950eefa288bebfe940198ba54bd246a52b772309
Kunmi Williams <kumswilliams@gmail.com>
2021-01-14 18:00:07 +0000

    post request

--- routes/users.test.js
+++ routes/users.test.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 // testing end point using SuperTest
 const db = require("../db");
 const app = require("../app");
@@ -26,27 +27,26 @@ describe("GET /users/:id", () => {
       role: "bootcamper",
       cohortno: 4,
       email: "participantview@gmail.com",
-      message: `You have added a new user`,
     });
   });
 
   describe("POST /users", () => {
     it("should post an object to the db", async () => {
-      const response = await request.post("/users");
+      const response = await request.post("/users").send({
+        bootcamperId: "1287",
+        firstName: "Test",
+        surname: "Test",
+        role: "bootcamper",
+        cohortNo: 4,
+        email: "participantview@gmail.com",
+      });
       expect(response.body).toMatchObject({
-        id: 56,
-        uuid: "8a6a31b4-1a6c-482a-bcfc-51cdba6bad10",
-        bootcamperid: "0000",
-        firstname: "Chris",
-        surname: "Meah",
-        role: "coach",
-        cohortno: 0,
-        email: "chris@chris.com",
+        message: `You have added a new user`,
       });
     });
   });
-  afterAll(async () => {
-    await db.close();
-    await app.close();
-  });
 });
+//   afterAll(async () => {
+//     await db.close();
+//     await app.close();
+//   });
